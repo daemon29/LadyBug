e663f3d05c574e8f0fefd3614a6e208df4d8f985
daemon29 <34591979+daemon29@users.noreply.github.com>
2019-03-21 08:11:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/donatetosave/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.donatetosave;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ImageView image;
+    FirebaseFirestore fb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= findViewById(R.id.button_camera);
+        image=findViewById(R.id.image_camera);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+        image.setImageBitmap(bitmap);
+    }
+}
