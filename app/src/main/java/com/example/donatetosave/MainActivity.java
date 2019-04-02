4756a5e53414bef63d883b51a36b6c893bf73d84
daemon29 <34591979+daemon29@users.noreply.github.com>
2019-04-02 23:22:08 +0700

    alpha-main_layout

--- app/src/main/java/com/example/donatetosave/MainActivity.java
+++ app/src/main/java/com/example/donatetosave/MainActivity.java
@@ -6,9 +6,15 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -20,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=(ImageButton)findViewById(R.id.btn_camera);
+        button=findViewById(R.id.btn_camera);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap =(Bitmap)data.getExtras().get("data");
+        Bitmap imageBitmap=(Bitmap)data.getExtras().get("data");
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup,null);
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable= true;
+        final PopupWindow popupWindow= new PopupWindow(popupView,width,height,focusable);
+        ImageView popupImage =(ImageView) findViewById(R.id.image_id);
+        popupWindow.showAtLocation(findViewById(R.id.linearlayout0), Gravity.CENTER,0,0);
+        popupImage.setImageBitmap(imageBitmap);
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return false;
+            }
+        });
     }
 }
