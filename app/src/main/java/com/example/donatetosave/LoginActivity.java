ee6c7463b570b9ce3dd03fcd00e3f24830e7659c
daemon29 <34591979+daemon29@users.noreply.github.com>
2019-05-24 13:56:05 +0700

    Decorate Login Layout

--- app/src/main/java/com/example/donatetosave/LoginActivity.java
+++ app/src/main/java/com/example/donatetosave/LoginActivity.java
@@ -51,8 +51,8 @@ public class LoginActivity extends Activity {
         // set the view now
 
         setContentView(R.layout.activity_login);
-        inputEmail =  findViewById(R.id.email);
-        inputPassword =  findViewById(R.id.password);
+        inputEmail =  findViewById(R.id.input_email);
+        inputPassword =  findViewById(R.id.input_password);
         progressBar =  findViewById(R.id.progressBar);
         btnSignup = findViewById(R.id.btn_signup);
         btnLogin = findViewById(R.id.btn_login);
@@ -104,7 +104,6 @@ public class LoginActivity extends Activity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (!task.isSuccessful()) {
-                                    // there was an error
                                     if (password.length() < 6) {
                                         inputPassword.setError(getString(R.string.minimum_password));
                                     } else {
