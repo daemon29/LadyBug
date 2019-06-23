da4570c921d7271185df2dea345085ddaa6a0e85
daemon29 <34591979+daemon29@users.noreply.github.com>
2019-06-23 07:36:33 +0700

    update

--- app/src/main/java/com/example/donatetosave/Adapter/NotificationAdapter.java
+++ app/src/main/java/com/example/donatetosave/Adapter/NotificationAdapter.java
@@ -1,5 +1,72 @@
 package com.example.donatetosave.Adapter;
 
-public class NotificationAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.donatetosave.Class.Achievement;
+import com.example.donatetosave.Class.Notification;
+import com.example.donatetosave.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class NotificationAdapter extends  FirestoreRecyclerAdapter<Notification,NotificationAdapter.NoteHolder> {
+    private Context mContext;
+
+    public NotificationAdapter(@NonNull FirestoreRecyclerOptions<Notification> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final NoteHolder holder, int position, @NonNull Notification model) {
+        String key = model.getKey();
+        FirebaseFirestore.getInstance().collection("Item").document(key).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                 String name = documentSnapshot.get("uid").toString();
+                 String tag = documentSnapshot.get("tag").toString();
+                 String url = documentSnapshot.get("url").toString();
+                 String content = documentSnapshot.get("description").toString();
+                 holder.Content.setText(content);
+                 holder.Name.setText("From :"+name);
+                 holder.Tag.setText(tag);
+                 Glide.with(mContext).load(url).into(holder.imageView);
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.notification_note, viewGroup, false);
+        this.mContext = viewGroup.getContext();
+
+        return new NoteHolder(v);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView Name, Tag, Content;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.notification_image);
+            Name = itemView.findViewById(R.id.notification_name);
+            Tag = itemView.findViewById(R.id.notification_tag);
+            Content = itemView.findViewById(R.id.notification_content);
+
+
+        }
+    }
 }
--- app/src/main/java/com/example/donatetosave/OtherUser.java
+++ app/src/main/java/com/example/donatetosave/OtherUser.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OtherUser extends AppCompatActivity {
     private ImageView BackGround, Image;
     private TextView Name, Bio,Email,WorkAt;
     private DocumentReference docref;
-
+    private Button addFriend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +33,10 @@ public class OtherUser extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle bundle = getIntent().getExtras();
-        String uid= bundle.getString("uid");
+        final String uid= bundle.getString("uid");
+        final String myuid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         Log.d("tag ",uid);
+        addFriend=findViewById(R.id.other_add);
         BackGround=findViewById(R.id.other_background);
         Image=findViewById(R.id.other_image);
         Name=findViewById(R.id.other_name);
@@ -47,7 +55,34 @@ public class OtherUser extends AppCompatActivity {
                 WorkAt.setText(Html.fromHtml("Work at: "+"<b>"+documentSnapshot.getString("work_at")+"</b>"));
             }
         });
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("name",documentSnapshot.get("name"));
+                        data.put("image_url",documentSnapshot.get("image_url"));
+                        data.put("uid",uid);
+                        FirebaseFirestore.getInstance().collection("User").document(myuid).collection("Friend").add(data);
 
+
+                    }
+                });
+                FirebaseFirestore.getInstance().collection("User").document(myuid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("name",documentSnapshot.get("name"));
+                        data.put("image_url",documentSnapshot.get("image_url"));
+                        data.put("uid",myuid);
+                        FirebaseFirestore.getInstance().collection("User").document(uid).collection("Friend").add(data);
+                    }
+                }) ;
+            }
+
+        });
 
     }
 }
